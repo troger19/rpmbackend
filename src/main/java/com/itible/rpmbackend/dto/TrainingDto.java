a70704ee6482e52d85140bd9bffb8c71c44a6549
jan.babel <jan.babel@emeldi.com>
2020-03-08 14:10:45 +0100

    add second for each RPM -> Map<Integer, Integer> rpm;

--- src/main/java/com/itible/rpmbackend/dto/TrainingDto.java
+++ src/main/java/com/itible/rpmbackend/dto/TrainingDto.java
@@ -5,13 +5,14 @@ import lombok.ToString;
 
 import java.math.BigDecimal;
 import java.util.Date;
-import java.util.List;
+import java.util.Map;
 
 @Data
 @ToString
 public class TrainingDto {
     private Date date;
-    private List<Integer> rpm;
+    //    private List<Integer> rpm;
+    private Map<Integer, Integer> rpm;
     private Integer duration;
     private BigDecimal avgRpm;
     private BigDecimal avgRpmTime;
